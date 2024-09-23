24e27914f7574e0ec1504653a93bbcef5381f85f
blsage <ben@sage.me>
2024-09-22 19:48:18 -0700

    Initial commit

---
+++ PhysicalIntelligence/App.swift
@@ -0,0 +1,17 @@
+//
+//  App.swift
+//  PhysicalIntelligence
+//
+//  Created by Benjamin Sage on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct PhysicalIntelligenceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
